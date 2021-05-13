3347b6f75d5c05c73650f4570ae93aab44165cd8
luispolippo <luispolippo@hotmail.com>
2021-05-13 16:06:59 -0300

    adicionado funções de editar e excluir do cadastro

--- src/main/java/curso/springboot/controller/PessoaController.java
+++ src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,8 +1,11 @@
 package curso.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,11 +23,13 @@ public class PessoaController {
 	private PessoaRepository pessoaRepository;
 
 	@GetMapping(value = "/cadastropessoa")
-	public String inicio() {
-		return PAGINACADASTRO;
+	public ModelAndView inicio() {
+		ModelAndView andView = new ModelAndView(PAGINACADASTRO);
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
 	}
 
-	@PostMapping(value = "/salvarpessoa")
+	@PostMapping(value = "**/salvarpessoa")
 	public ModelAndView salvar(Pessoa pessoa) {
 
 		pessoaRepository.save(pessoa);
@@ -33,14 +38,35 @@ public class PessoaController {
 
 	}
 
-	@GetMapping(value = "/listapessoas")
+	@GetMapping(value = "**/listapessoas")
 	public ModelAndView listapessoas() {
 		
 		ModelAndView andView = new ModelAndView(PAGINACADASTRO);
 		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
 		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
 
 		return andView;
 	}
-
+	
+	@GetMapping(value = "/editarpessoa/{idpessoa}")
+	public ModelAndView editarpessoa(@PathVariable("idpessoa") Long idpessoa) {
+		
+		ModelAndView andView = new ModelAndView(PAGINACADASTRO);
+		
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+		andView.addObject("pessoaobj", pessoa.get());
+		
+		return andView;
+	}
+	
+	@GetMapping(value = "/excluirpessoa/{idpessoa}")
+	public ModelAndView excluirpessoa(@PathVariable("idpessoa") Long idpessoa) {
+		
+		pessoaRepository.deleteById(idpessoa);
+		
+		return listapessoas();
+		
+	}
+		
 }
